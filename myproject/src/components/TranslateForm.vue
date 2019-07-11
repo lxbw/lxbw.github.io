c2b1e178695e9dc90a14aa53e3f7728f4302940c
lxbw <l8527@163.com>
2019-07-11 11:42:28 +0800

    小项目

---
+++ myproject/src/components/TranslateForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="translateForm">
+    
+    <form v-on:submit="formSubmit">
+        <input type="text" v-model="TextToTranslate" placeholder="输入需要翻译的内容">
+        <select v-model="langguage">
+            <option value="en">English</option>
+            <option value="ru">Russian</option>
+            <option value="ko">Korean</option>
+             <option value="ja">Janpenese</option> 
+        </select>
+        <input type="submit" value="翻译">
+    </form>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'translateForm',
+  data:function(){
+      return{
+          TextToTranslate:"",langguage:""
+          
+
+      }
+  },
+  methods:{
+      formSubmit:function(e){
+        //  alert(this.TextToTranslate);
+        this.$emit("formSubmit",this.TextToTranslate,this.langguage)
+          e.preventDefault();
+  
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
